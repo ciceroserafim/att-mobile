a8ca1f4695b214389f99e0eec424a87844e28b24
ciceroserafim <ciceroserafim777@gmail.com>
2025-08-17 04:39:53 -0300

    att

--- App.tsx
+++ App.tsx
@@ -1,20 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, FlatList, Text, View, ActivityIndicator, StyleSheet } from "react-native";
+import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
 
+//Instância do QueryClient
+const queryClient = new QueryClient();
+
+// Função para buscar usuários
+const fetchUsers = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  if (!response.ok) {
+    throw new Error("Erro ao buscar usuários");
+  }
+  return response.json();
+};
+
+// Componente que mostra os usuários
+function UsersList() {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["users"],
+    queryFn: fetchUsers,
+  });
+
+  if (isLoading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" color="#007AFF" />
+        <Text>Carregando usuários...</Text>
+      </View>
+    );
+  }
+
+  if (isError) {
+    return (
+      <View style={styles.center}>
+        <Text style={{ color: "red" }}>Erro ao carregar usuários</Text>
+      </View>
+    );
+  }
+
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => (
+        <View style={styles.card}>
+          <Text style={styles.name}>{item.name}</Text>
+          <Text>Email: {item.email}</Text>
+          <Text>Cidade: {item.address.city}</Text>
+        </View>
+      )}
+    />
+  );
+}
+
+// App principal com QueryClientProvider
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Lista de Usuários</Text>
+        <UsersList />
+      </SafeAreaView>
+    </QueryClientProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 40,
+    paddingHorizontal: 10,
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  card: {
+    backgroundColor: "#FFC4CA",
+    padding: 15,
+    marginVertical: 8,
+    borderRadius: 8,
+    elevation: 2,
+  
+  },
+  name: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
+
